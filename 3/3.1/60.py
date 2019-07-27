155eb054fbc60ba54c7c07ac97fd48f293c29773
dengxiongshi <2372285241@qq.com>
2019-07-27 09:38:14 +0800

    完成代码60-63

--- 3/3.1/60.py
+++ 3/3.1/60.py
@@ -53,14 +53,50 @@ xx_poly2 = poly2.transform(xx)
 yy_poly2 = regressor_poly2.predict(xx_poly2)
 
 # 分别对训练数据点、线性回归直线、2次多项式回归曲线进行作图。
-plt.scatter(X_train, y_train)
-plt1, = plt.plot(xx, yy, label='Degree=1')
-plt2, = plt.plot(xx, yy_poly2, label='Degree=2')
+# plt.scatter(X_train, y_train)
+# plt1, = plt.plot(xx, yy, label='Degree=1')
+# plt2, = plt.plot(xx, yy_poly2, label='Degree=2')
+#
+# plt.axis([0, 25, 0, 25])
+# plt.xlabel('Diameter of Pizza')
+# plt.ylabel('Price of Pizza')
+# plt.legend(handles=[plt1, plt2])
+# plt.show()
+# 输出2次多项式回归回归模型在训练样本上的R-squared值。
+print('The R-squared value of Polynomial Regressor performing (Degree=2) performing on the training data is', regressor_poly2.score(X_train_poly2, y_train))
 
+"""使用4次多项式回归模型在披萨饼训练样本上进行拟合"""
+from sklearn.preprocessing import PolynomialFeatures
+
+poly4 = PolynomialFeatures(degree=4)
+X_train_poly4 = poly4.fit_transform(X_train)
+regressor_poly4 = LinearRegression()
+# 对4次多项式回归模型进行训练。
+regressor_poly4.fit(X_train_poly4, y_train)
+
+# 从新映射绘图用x轴采用数据。
+xx_poly4 = poly4.transform(xx)
+yy_poly4 = regressor_poly4.predict(xx_poly4)
+# # 分别对训练数据点、线性回归直线、2次多项式以及4次多项式回归曲线进行作图。
+plt.scatter(X_train, y_train)
+plt1,= plt.plot(xx, yy, label='Degree=1')
+plt2,= plt.plot(xx, yy_poly2, label='Degree=2')
+plt4,= plt.plot(xx, yy_poly4, label='Degree=4')
 plt.axis([0, 25, 0, 25])
 plt.xlabel('Diameter of Pizza')
 plt.ylabel('Price of Pizza')
-plt.legend(handles=[plt1, plt2])
+plt.legend(handles=[plt1, plt2, plt4])
 plt.show()
-# 输出2次多项式回归回归模型在训练样本上的R-squared值。
-print('The R-squared value of Polynomial Regressor performing (Degree=2) performing on the training data is', regressor_poly2.score(X_train_poly2, y_train))
+
+print('The R-squared value of Polynomial Regressor performing (Degree=4) performing on the training data is', regressor_poly4.score(X_train_poly4, y_train))
+
+"""评估3种回归模型在测试数据集上的性能表现"""
+X_test = [[6], [8], [11], [16]]
+y_test = [[8], [12], [15], [18]]
+print(lr.score(X_test, y_test))
+# 使用测试数据对2次多项式回归模型的性能进行评估
+X_test_poly2 = poly2.transform(X_test)
+print(regressor_poly2.score(X_test_poly2, y_test))
+# 使用测试数据对4次多项式回归模型的性能进行评估
+X_test_poly4 = poly4.transform(X_test)
+print(regressor_poly4.score(X_test_poly4, y_test))
